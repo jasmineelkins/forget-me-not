bc202fa6d8f198347e8ccd8b639dd1a7152e1f4b
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-06-01 15:09:32 -0400

    updated BASE_URL

--- client/src/Config.js
+++ client/src/Config.js
@@ -2,6 +2,6 @@
 // const BASE_URL = "";
 
 // for production:
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://jasmineelkins-forget-me-not.herokuapp.com/signup";
 
 export default BASE_URL;
