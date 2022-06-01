11d516d2a9ef963354a940a63a41b745fb7cdeed
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-06-01 15:45:55 -0400

    updated BASE_URL typo

--- client/src/Config.js
+++ client/src/Config.js
@@ -2,6 +2,6 @@
 // const BASE_URL = "";
 
 // for production:
-const BASE_URL = "https://jasmineelkins-forget-me-not.herokuapp.com/signup";
+const BASE_URL = "https://jasmineelkins-forget-me-not.herokuapp.com";
 
 export default BASE_URL;
